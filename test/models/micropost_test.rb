e0f8377a9349d1c81533028a9f50af78a531a131
Shivam Sinha <shivams@qubole.com>
2018-06-18 18:38:04 +0530

    blog

---
+++ test/models/micropost_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+
+  def setup
+    @user = users(:michael)
+    # This code is not idiomatically correct.
+    @micropost = Micropost.new(content: "Lorem ipsum")
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+end
